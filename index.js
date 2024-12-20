8e9c226854acfa1d72cc0c3d6f13e6829339997b
Elavarasan BUI0001 <elavarasan@brightuitechnologies.com>
2024-12-20 11:54:04 +0530

    mysql operations

--- index.js
+++
@@ -1,4 +0,0 @@
-const {format } =  require("date-fns");
-const { v4: uuidv4 } = require('uuid');
-console.log(format(new Date(), "yyyy-MM-dd HH:mm:ss"));
-console.log(uuidv4())
--- server_ES6.mjs
+++
@@ -1,6 +0,0 @@
-import {add,sub,mul,div} from './math.js';
-console.log('----------------------Math Functions ------------------------------');
-console.log(add(4,2));
-console.log(sub(4,2));
-console.log(mul(4,2));
-console.log(div(4,3));
